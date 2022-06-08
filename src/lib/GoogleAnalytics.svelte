96786871c88530c9bbb6f5640281660496991558
jakzo <jack@jf.id.au>
2022-06-08 20:57:25 +1000

    analytics

---
+++ src/lib/GoogleAnalytics.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-HMXFLBMBFD"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-HMXFLBMBFD");
+  </script>
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,14 @@
 <script>
   import "../app.scss";
   import Nav from "$lib/Nav.svelte";
+  import GoogleAnalytics from "$lib/GoogleAnalytics.svelte";
 </script>
 
 <svelte:head>
   <meta property="og:site_name" content="jakzo" />
 </svelte:head>
+
+<GoogleAnalytics />
 
 <Nav />
 <slot />
